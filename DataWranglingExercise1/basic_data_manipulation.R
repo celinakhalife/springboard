60d2839eb5f9ee4766132ea2634cbcc0bd77b0c3
Celina Khalife <celina.khalife@gmail.com>
2019-03-27 21:11:18 +0200

    fix: DW_1 read add documentation

--- DataWranglingExercise1/basic_data_manipulation.R
+++ DataWranglingExercise1/basic_data_manipulation.R
@@ -1,7 +1,10 @@
 #load refine dataset
 refine <- read.csv('./DataWranglingExercise1/refine_original.csv')
 
+# supposedly, we have the list of all brands
 differentBrands = c("phillips", "akzo", "van houten", "unilever")
+# I', searching for matches to the differentBrands list and 
+# replacing the company value with it 
 refine$company <- sapply(refine$company,
                          function(val) {
                            agrep(tolower(val),
