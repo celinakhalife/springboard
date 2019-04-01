fe8b43861673588dc74a8b6fe9750b6a5d274837
Celina Khalife <celina.khalife@gmail.com>
2019-04-01 23:42:43 +0300

    feat: DW_2 replace empty age values with age mean

--- DataWranglingExercise2/dealing_with_missing_values.R
+++ DataWranglingExercise2/dealing_with_missing_values.R
@@ -6,3 +6,7 @@ data <- read.csv('./DataWranglingExercise2/titanic_original.csv')
 
 # replace empty embarked values with S
 data$embarked[data$embarked == ''] <- "S"
+
+# fill empty age values with age mean
+ageMean = mean(data$age, na.rm = TRUE)
+data$age[is.na(data$age)] <- ageMean
