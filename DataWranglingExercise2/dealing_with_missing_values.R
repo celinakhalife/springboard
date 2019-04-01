df0d29b71e88c28d020ee31e26f82fb43b3c41d1
Celina Khalife <celina.khalife@gmail.com>
2019-04-01 23:54:06 +0300

    feat: DW_2 replace empty boat values with NA

--- DataWranglingExercise2/dealing_with_missing_values.R
+++ DataWranglingExercise2/dealing_with_missing_values.R
@@ -2,7 +2,7 @@ library(tidyr)
 library(dplyr)
 
 #load refine dataset
-data <- read.csv('./DataWranglingExercise2/titanic_original.csv')
+data <- read.csv('./DataWranglingExercise2/titanic_original.csv', stringsAsFactors = FALSE)
 
 # replace empty embarked values with S
 data$embarked[data$embarked == ''] <- "S"
@@ -10,3 +10,7 @@ data$embarked[data$embarked == ''] <- "S"
 # fill empty age values with age mean
 ageMean = mean(data$age, na.rm = TRUE)
 data$age[is.na(data$age)] <- ageMean
+
+# replace empty boat  values with the string NA
+data$boat[data$boat == ''] <- "NA"
+
